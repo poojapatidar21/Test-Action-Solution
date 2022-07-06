34232ed4756a944000e7d583cd5b5365be662051
poojapatidar21 <poojapatidar897@gmail.com>
2022-07-06 17:46:37 +0530

    changes11

--- src/action.js
+++ src/action.js
@@ -4,6 +4,8 @@ const github=require('@actions/github');
 try {
     const name=core.getInput('who-to-greet');
     console.log(`Hello ${name}`);
+    const ConnectedServiceName=core.getInput('ConnectedServiceName');
+    console.log(`Hello ${ConnectedServiceName}`);
 
     const time=(new Date()).toTimeString();
     core.setOutput("time",time);
