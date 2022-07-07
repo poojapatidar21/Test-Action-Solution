10116026515cf9584401cd261fdb3169fad0f685
poojapatidar21 <poojapatidar897@gmail.com>
2022-07-07 19:43:17 +0530

    changes12-v1

--- src/action.js
+++ src/action.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,40 +8,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(require("@actions/core"));
+exports.run = void 0;
+const core_1 = __importDefault(require("@actions/core"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ConnectedServiceName = core.getInput('ConnectedServiceName');
-            console.log(ConnectedServiceName);
-            const Intent = core.getInput('Intent');
-            console.log(Intent);
-            const ContentType = core.getInput('ContentType');
-            console.log(ContentType);
-            const PackageLocation = core.getInput('PackageLocation');
-            console.log(PackageLocation);
-            const Owners = core.getInput('Owners');
-            console.log(Owners);
-            const Approvers = core.getInput('Approvers');
-            console.log(Approvers);
-            const ServiceEndpointUrl = core.getInput('ServiceEndpointUrl');
-            console.log(ServiceEndpointUrl);
-            const MainPublisher = core.getInput('MainPublisher');
-            console.log(MainPublisher);
-            const DomainTenantId = core.getInput('DomainTenantId');
-            console.log(DomainTenantId);
-            const ContentOrigin = core.getInput('ContentOrigin');
-            console.log(ContentOrigin);
-            const ProductState = core.getInput('ProductState');
-            console.log(ProductState);
-            const Audience = core.getInput('Audience');
-            console.log(Audience);
+            const ConnectedServiceName = core_1.default.getInput('ConnectedServiceName');
+            if (ConnectedServiceName == 'bad' || ConnectedServiceName == undefined) {
+                console.log("Invalid ConnecterServiceName");
+                throw new Error("Invalid ConnecterServiceName for" + ConnectedServiceName);
+            }
+            else {
+                console.log(ConnectedServiceName);
+            }
         }
         catch (error) {
-            if (error instanceof Error)
-                core.setFailed(error.message);
+            console.log(error);
         }
     });
 }
-run();
+exports.run = run;
--- src/action.ts
+++ src/action.ts
@@ -1,47 +1,18 @@
-import * as core from '@actions/core'
-
-async function run():Promise<void> {
-    try {
-        const ConnectedServiceName:string= core.getInput('ConnectedServiceName')
-        console.log(ConnectedServiceName)
-
-        const Intent:string= core.getInput('Intent')
-        console.log(Intent)
-
-        const ContentType:string= core.getInput('ContentType')
-        console.log(ContentType)
-
-        const PackageLocation:string= core.getInput('PackageLocation')
-        console.log(PackageLocation)
-
-        const Owners:string= core.getInput('Owners')
-        console.log(Owners)
-
-        const Approvers:string= core.getInput('Approvers')
-        console.log(Approvers)
-
-        const ServiceEndpointUrl:string= core.getInput('ServiceEndpointUrl')
-        console.log(ServiceEndpointUrl)
-
-        const MainPublisher:string= core.getInput('MainPublisher')
-        console.log(MainPublisher)
-
-        const DomainTenantId:string= core.getInput('DomainTenantId')
-        console.log(DomainTenantId)
-
-        const ContentOrigin:string= core.getInput('ContentOrigin')
-        console.log(ContentOrigin)
-
-        const ProductState:string= core.getInput('ProductState')
-        console.log(ProductState)
-
-        const Audience:string= core.getInput('Audience')
-        console.log(Audience)
+import core from "@actions/core"
+export async function run(this:any) {
+    try{
+        const ConnectedServiceName:string = core.getInput('ConnectedServiceName')
+        if (ConnectedServiceName == 'bad' || ConnectedServiceName==undefined){
+            console.log("Invalid ConnecterServiceName")
+            throw new Error( "Invalid ConnecterServiceName for"+ConnectedServiceName )
+        }
+        else{
+            console.log(ConnectedServiceName)
+        }
 
 
-    } catch (error){
-        if(error instanceof Error) core.setFailed(error.message)
+    } catch(error){
+        console.log(error)
     }
+    
 }
-
-run()
