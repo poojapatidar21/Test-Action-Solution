f19b251ba3a6c2510e1f6e29d7240875141dd9bf
poojapatidar21 <poojapatidar897@gmail.com>
2022-07-07 20:25:38 +0530

    changes12-v5

--- src/action.js
+++ src/action.js
@@ -19,15 +19,10 @@ function run() {
         try {
             const ConnectedServiceName = core_1.default.getInput('ConnectedServiceName');
             console.log(ConnectedServiceName);
-            if (ConnectedServiceName == '72f988bf-86f1-41af-91ab-2d7cd011db47') {
-                console.log("Valid ConnecterServiceName");
-            }
-            else {
-                console.log("Invalid ConnecterServiceName" + ConnectedServiceName);
-            }
         }
         catch (error) {
-            console.log(error);
+            if (error instanceof Error)
+                core_1.default.setFailed(error.message);
         }
     });
 }
--- src/action.ts
+++ src/action.ts
@@ -4,16 +4,9 @@ export async function run() {
     try{
         const ConnectedServiceName:string = core.getInput('ConnectedServiceName')
         console.log(ConnectedServiceName)
-        if (ConnectedServiceName == '72f988bf-86f1-41af-91ab-2d7cd011db47' ){
-            console.log("Valid ConnecterServiceName")
-        }
-        else{
-            console.log("Invalid ConnecterServiceName"+ConnectedServiceName)
-        }
-
-
-    } catch(error){
-        console.log(error)
+        
+    } catch (error){
+        if(error instanceof Error) core.setFailed(error.message)
     }
     
 }
