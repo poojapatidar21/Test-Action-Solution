85a3af9578ffa8b9bf199c3cd2b1d038497b3ab6
poojapatidar21 <poojapatidar897@gmail.com>
2022-07-10 16:51:01 +0530

    changes15-v2

--- src/Core/configManager.ts
+++ src/Core/configManager.ts
@@ -55,7 +55,7 @@ export class ConfigManager{
             this.config.KVIdentityConfig.KeyVaultName= "Keyvault-2-basic"
             this.config.KVIdentityConfig.AuthCertName= "Package-Maven"
             this.config.KVIdentityConfig.ClientId= "1f4aeb8e-6298-435d-a918-e2f4c0d62089"
-            this.config.KVIdentityConfig.ClientSecret= process.env['PASSWORD-SECRET']
+            this.config.KVIdentityConfig.ClientSecret= "ba243820-6d27-4069-87fd-959715a73504"
             // this.config.KVIdentityConfig.SignCertName= "xyz"
         }
         else {
