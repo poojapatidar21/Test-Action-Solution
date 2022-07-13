4da2cd4606c6a9f4935508468dd90803838eb19a
poojapatidar21 <poojapatidar897@gmail.com>
2022-07-13 13:14:31 +0530

    setting env variables

--- src/Common/authContextManager.ts
+++ src/Common/authContextManager.ts
@@ -33,7 +33,7 @@ export class AuthenticationManager implements IAuthenticationManager{
             }
         }
         console.log(clientConfig)
-        
+        console.log("hello")
         const cca = new Msal.ConfidentialClientApplication(clientConfig) 
         console.log(this.accessToken, "accessToken")
         var gatewayScope = resourceUri + Constant.APIAccessDefaultScope 
