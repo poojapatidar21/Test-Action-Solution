4bbc86d73f8d306903396748f98f8b21eb57aa15
poojapatidar21 <poojapatidar897@gmail.com>
2022-07-13 11:17:12 +0530

    adding certs Info as secret

--- src/Core/configManager.ts
+++ src/Core/configManager.ts
@@ -26,21 +26,21 @@ export class ConfigManager{
 
     private setConfigVariables(){
        
-        this.config.DomainTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
-        this.config.ServiceEndpointUrl = "https://ppe.api.esrp.microsoft.com";
-        // this.config.AppInsightsLoggingKey = "33e01921-4d64-4f8c-a055-5bdaffd5e33d";
-        this.config.MainPublisher = "ESRPRELTEST";
-        this.config.Intent = "PackageDistribution";
-        this.config.ContentType = "Maven";
-        this.config.ContentOrigin = "azeus";
-        this.config.ProductState = "new";
-        this.config.Audience = "Workflow.A_S_AV_PackageManager";
-        this.config.Environment = "Developer";
-        this.config.PackageLocation = "src/Tasks/github.Release.Task/pacman-app-1.1";
-        this.config.Owners = "xyz@microsoft.com";
-        this.config.Approvers = "abc@microsoft.com";
-        this.config.StatusPollingInterval = Constant.DelayBetweenEveryGetStatus;
-        this.config.ConnectedServiceName = "ReleaseServiceConnection";
+        this.config.DomainTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+        this.config.ServiceEndpointUrl='https://api.esrp.microsoft.com'
+        // this.config.AppInsightsLoggingKey = "33e01921-4d64-4f8c-a055-5bdaffd5e33d"
+        this.config.MainPublisher = "ESRPRELTEST"
+        this.config.Intent = "PackageDistribution"
+        this.config.ContentType = "Maven"
+        this.config.ContentOrigin = "azeus"
+        this.config.ProductState = "new"
+        this.config.Audience = "Workflow.A_S_AV_PackageManager"
+        this.config.Environment = "Developer"
+        this.config.PackageLocation = "src/Tasks/github.Release.Task/pacman-app-1.1"
+        this.config.Owners = "xyz@microsoft.com"
+        this.config.Approvers = "abc@microsoft.com"
+        this.config.StatusPollingInterval = Constant.DelayBetweenEveryGetStatus
+        this.config.ConnectedServiceName = "ReleaseServiceConnection"
 
         if (this.config.ConnectedServiceName == Constant.Bad || this.config.ConnectedServiceName == undefined) {
 
@@ -54,8 +54,8 @@ export class ConfigManager{
             this.config.KVIdentityConfig.TenantId= "eb2b6278-b96d-4200-89a6-bcd387294884"
             this.config.KVIdentityConfig.KeyVaultName= "esrprelease-test-prod"
             this.config.KVIdentityConfig.AuthCertName= "74cd0b2a-8473-475e-9bfd-445ec0847a84"
-            this.config.KVIdentityConfig.ClientId= "1f4aeb8e-6298-435d-a918-e2f4c0d62089"
-            this.config.KVIdentityConfig.ClientSecret= "tsp7Q~QAE-xXLsBxYy0au6bYZ~d07ibr7viKP"
+            this.config.KVIdentityConfig.ClientId= process.env["KVAUTHCLIENT"]
+            this.config.KVIdentityConfig.ClientSecret= process.env["KVAUTHSECRET"]
             this.config.KVIdentityConfig.SignCertName= "74cd0b2a-8473-475e-9bfd-445ec0847a84"
         }
         else {
